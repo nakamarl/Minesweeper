643a9347415889ae798c4547234ecf0f9aec7781
s17007 <s17007@std.it-college.ac.jp>
2018-12-06 12:44:14 +0900

    盤面生成まで

--- app/src/main/java/jp/ac/it_college/std/s17007/minesweeper/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s17007/minesweeper/MainActivity.kt
@@ -9,12 +9,69 @@ import android.graphics.Paint
 import android.graphics.Point
 import android.util.DisplayMetrics
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import jp.ac.it_college.std.s17007.minesweeper.R.id.field
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    val ROW = 9
+    val COLUMN = 9
+    val MINE_COUNT = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createView()
+
+    }
+
+    private fun createView(){
         setContentView(R.layout.activity_main)
+        generateField()
+        val button = findViewById<Button>(R.id.resetTextView)
+
+        button.setOnClickListener{
+            createView()
+        }
+    }
+
+    private fun generateField(){
+        //0:kihon 1~8:num of near mine 9:mine
+        var mineField = Array(COLUMN,{ arrayOfNulls<String>(ROW) })
+        var i = 0
+        var j = 0
+        for(i in 0..8){
+            for(j in 0..8){
+                mineField[i][j] = " "
+            }
+        }
+        val rand = Random()
+        var count = 0
+        while(count < MINE_COUNT){
+            var x = rand.nextInt(9)
+            var y = rand.nextInt(9)
+
+            if(mineField[x][y] != "*"){
+                mineField[x][y] = "*"
+                count++
+            }
+        }
+
+        var id = 0
+        var idString = "textview"
+        for(i in 0..8){
+            for(j in 0..8){
+                idString += id.toString()
+                val callid = resources.getIdentifier(idString,"id",packageName)
+                val cell = findViewById<TextView>(callid)
+                cell.setText(mineField[i][j].toString())
+                idString = "textview"
+                id++
+            }
+        }
+
     }
 }
 
